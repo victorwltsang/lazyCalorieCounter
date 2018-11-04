e97919d821475725a8fa374433a7b79807943087
allen wong <allencwwong@gmail.com>
2018-11-04 14:08:57 -0800

    list page style

--- src/components/AddEntry/AddEntry.jsx
+++ src/components/AddEntry/AddEntry.jsx
@@ -1,257 +1,207 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-import { Typography, Button, Divider } from '@material-ui/core';
-import FileBase64 from 'react-file-base64';
-import TextField from '@material-ui/core/TextField';
-import moment from 'moment';
-import Clarifai from 'clarifai';
-import IngredientButton from './IngredientButton/IngredientButton';
-import api from '../../utils/api';
+import { Typography, Button, Divider } from "@material-ui/core";
+import FileBase64 from "react-file-base64";
+import TextField from "@material-ui/core/TextField";
+import moment from "moment";
+import Clarifai from "clarifai";
+import IngredientButton from "./IngredientButton/IngredientButton";
+import api from "../../utils/api";
 const clarifaiApp = new Clarifai.App({
-	apiKey: 'aa6af86e8861469684d627709efaaa25'
+  apiKey: "aa6af86e8861469684d627709efaaa25"
 });
-console.log('hi', process.env.FAUNADB_SECRET);
+console.log("hi", process.env.FAUNADB_SECRET);
 const styles = theme => ({
-	root: {
-		padding: '25px 50px'
-	},
-	paper: {
-		padding: theme.spacing.unit * 1,
-		textAlign: 'center',
-		color: theme.palette.text.secondary
-	},
-	formWrapper: {
-		marginTop: '10px'
-	},
-	iconSmall: {
-		marginRight: 5
-	},
-	button: {
-		width: '100%',
-		fontSize: '20px',
-		margin: 'auto',
-		padding: '15px'
-	},
-	ingredientsWrapper: {
-		marginTop: '14px',
-		padding: 20,
-		backgroundColor: 'transparent',
-		border: '1px solid #b7b7b7',
-		borderRadius: '5px',
-		display: 'flex',
-		flexWrap: 'wrap'
-	},
-	img: {
-		marginTop: '10px',
-		borderRadius: '5px',
-		width: '100%'
-	}
+  root: {
+    padding: "25px 50px"
+  },
+  paper: {
+    padding: theme.spacing.unit * 1,
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  formWrapper: {
+    marginTop: "10px"
+  },
+  iconSmall: {
+    marginRight: 5
+  },
+  button: {
+    width: "100%",
+    fontSize: "20px",
+    margin: "auto",
+    padding: "15px"
+  },
+  ingredientsWrapper: {
+    marginTop: "14px",
+    padding: 20,
+    backgroundColor: "transparent",
+    border: "1px solid #b7b7b7",
+    borderRadius: "5px",
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  img: {
+    marginTop: "10px",
+    borderRadius: "5px",
+    width: "100%"
+  }
 });
 
 class AddEntry extends Component {
-	state = {
-		title: '',
-		date: '',
-		files: [],
-		ingredients: []
-	};
+  state = {
+    title: "",
+    date: "",
+    files: [],
+    ingredients: []
+  };
 
-	componentDidMount() {
-		console.log('loaded');
-		let today = moment(new Date()).format('YYYY-MM-DD');
-		this.setState({
-			date: today
-		});
-	}
+  componentDidMount() {
+    console.log("loaded");
+    let today = moment(new Date()).format("YYYY-MM-DD");
+    this.setState({
+      date: today
+    });
+  }
 
-	handleChange = name => event => {
-		this.setState({
-			[name]: event.target.value
-		});
-	};
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
 
-	saveEntry = () => {
-		let entry = {
-			title: this.state.title,
-			date: this.state.date,
-			fileName: this.state.files.name,
-			fileBase: this.state.files.base64
-		};
+  saveEntry = () => {
+    let entry = {
+      title: this.state.title,
+      date: this.state.date,
+      fileName: this.state.files.name,
+      fileBase: this.state.files.base64
+    };
 
-		console.log(entry);
+    console.log(entry);
 
-		api.create(entry).then(response => {
-			console.log(response);
-			// got back to list page
-		});
-	};
+    api.create(entry).then(response => {
+      console.log(response);
+      // got back to list page
+    });
+  };
 
-	async getFiles(files) {
-		this.setState({ files: files });
-		// clarifaiApp here
+  async getFiles(files) {
+    this.setState({ files: files });
+    // clarifaiApp here
 
-		var base64Img = this.state.files.base64.split(',')[1];
+    var base64Img = this.state.files.base64.split(",")[1];
 
-		let clarifaiResponse = await clarifaiApp.models
-			.predict(Clarifai.FOOD_MODEL, { base64: base64Img })
-			.then(response => {
-				return response.rawData.outputs[0].data.concepts.map(concept => {
-					return concept.name;
-				});
-			});
+    let clarifaiResponse = await clarifaiApp.models
+      .predict(Clarifai.FOOD_MODEL, { base64: base64Img })
+      .then(response => {
+        return response.rawData.outputs[0].data.concepts.map(concept => {
+          return concept.name;
+        });
+      });
 
-		console.log(clarifaiResponse);
-		this.setState({
-			ingredients: clarifaiResponse
-		});
-	}
+    console.log(clarifaiResponse);
+    this.setState({
+      ingredients: clarifaiResponse
+    });
+  }
 
-	// save entry
+  render() {
+    const { classes } = this.props;
+    console.log(this.state);
+    console.log(this.state.ingredients);
+    let ingredientsButtons = this.state.ingredients.map(ing => {
+      return <IngredientButton key={ing} name={ing} />;
+    });
+    return (
+      <Paper className={classes.root}>
+        <Typography align="center" variant="h5">
+          Add New Entry
+        </Typography>
+        <Grid container spacing={24} className={classes.formWrapper}>
+          <Grid item xs={12}>
+            <form className={classes.container} noValidate autoComplete="off">
+              <TextField
+                id="title"
+                label="Title"
+                value={this.state.title}
+                onChange={this.handleChange("title")}
+                placeholder="Title/Description"
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+              <TextField
+                id="date"
+                label="Date"
+                type="date"
+                value={this.state.date}
+                onChange={this.handleChange("date")}
+                fullWidth
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+            </form>
+          </Grid>
+          <Grid item xs={12}>
+            {/* <FilePond allowFileEncod="true" /> */}
+          </Grid>
 
-	// saveTodo = e => {
-	//     e.preventDefault();
-	//     const { todos } = this.state;
-	//     const todoValue = this.inputElement.value;
+          <Grid item xs={12}>
+            <Grid container spacing={24}>
+              <Grid item xs={12} md={6}>
+                <FileBase64 onDone={this.getFiles.bind(this)} />
+                <img src={this.state.files.base64} className={classes.img} />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography variant="caption">Ingredients</Typography>
+                <div className={classes.ingredientsWrapper}>
+                  {ingredientsButtons}
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
 
-	//     if (!todoValue) {
-	//       alert("Please add Todo title");
-	//       this.inputElement.focus();
-	//       return false;
-	//     }
-
-	//     // reset input to empty
-	//     this.inputElement.value = "";
-
-	//     const todoInfo = {
-	//       title: todoValue,
-	//       completed: false
-	//     };
-	//     // Optimistically add todo to UI
-	//     const newTodoArray = [
-	//       {
-	//         data: todoInfo,
-	//         ts: new Date().getTime() * 10000
-	//       }
-	//     ];
-
-	//     const optimisticTodoState = newTodoArray.concat(todos);
-
-	//     this.setState({
-	//       todos: optimisticTodoState
-	//     });
-	//     // Make API request to create new todo
-	//     api
-	//       .create(todoInfo)
-	//       .then(response => {
-	//         console.log(response);
-	//         // remove temporaryValue from state and persist API response
-	//         const persistedState = removeOptimisticTodo(todos).concat(response);
-	//         // Set persisted value to state
-	//         this.setState({
-	//           todos: persistedState
-	//         });
-	//       })
-	//       .catch(e => {
-	//         console.log("An API error occurred", e);
-	//         const revertedState = removeOptimisticTodo(todos);
-	//         // Reset to original state
-	//         this.setState({
-	//           todos: revertedState
-	//         });
-	//       });
-	//   };
-
-	//
-
-	render() {
-		const { classes } = this.props;
-		console.log(this.state);
-		console.log(this.state.ingredients);
-		let ingredientsButtons = this.state.ingredients.map(ing => {
-			return <IngredientButton key={ing} name={ing} />;
-		});
-		return (
-			<Paper className={classes.root}>
-				<Typography align="center" variant="h5">
-					Add New Entry
-				</Typography>
-				<Grid container spacing={24} className={classes.formWrapper}>
-					<Grid item xs={12}>
-						<form className={classes.container} noValidate autoComplete="off">
-							<TextField
-								id="title"
-								label="Title"
-								value={this.state.title}
-								onChange={this.handleChange('title')}
-								placeholder="Title/Description"
-								fullWidth
-								margin="normal"
-								variant="outlined"
-								InputLabelProps={{
-									shrink: true
-								}}
-							/>
-							<TextField
-								id="date"
-								label="Date"
-								type="date"
-								value={this.state.date}
-								onChange={this.handleChange('date')}
-								fullWidth
-								margin="normal"
-								variant="outlined"
-								InputLabelProps={{
-									shrink: true
-								}}
-							/>
-						</form>
-					</Grid>
-					<Grid item xs={12}>
-						{/* <FilePond allowFileEncod="true" /> */}
-					</Grid>
-
-					<Grid item xs={12}>
-						<Grid container spacing={24}>
-							<Grid item xs={12} md={6}>
-								<FileBase64 onDone={this.getFiles.bind(this)} />
-								<img src={this.state.files.base64} className={classes.img} />
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<Typography variant="caption">Ingredients</Typography>
-								<div className={classes.ingredientsWrapper}>{ingredientsButtons}</div>
-							</Grid>
-						</Grid>
-					</Grid>
-
-					<Grid item xs={12}>
-						<Grid container spacing={24}>
-							<Grid item xs={12} sm={6}>
-								<Button
-									variant="contained"
-									size="large"
-									color="primary"
-									className={classes.button}
-									onClick={this.saveEntry}
-									disabled={this.state.files.length === 0 ? true : false}
-								>
-									Save
-								</Button>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<Button variant="contained" size="large" color="secondary" className={classes.button}>
-									Cancel
-								</Button>
-							</Grid>
-						</Grid>
-					</Grid>
-				</Grid>
-			</Paper>
-		);
-	}
+          <Grid item xs={12}>
+            <Grid container spacing={24}>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  className={classes.button}
+                  onClick={this.saveEntry}
+                  disabled={this.state.files.length === 0 ? true : false}
+                >
+                  Save
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="secondary"
+                  className={classes.button}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  }
 }
 
 export default withStyles(styles)(AddEntry);
--- src/components/ListDiary/ListDiary.jsx
+++ src/components/ListDiary/ListDiary.jsx
@@ -1,43 +1,92 @@
-import React, { Component } from 'react';
-import api from '../../utils/api';
-import isLocalHost from '../../utils/isLocalHost';
+import React, { Component } from "react";
+import api from "../../utils/api";
+import isLocalHost from "../../utils/isLocalHost";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: "100%",
+    height: "auto"
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)"
+  }
+});
+
 class ListDiary extends Component {
-	state = {
-		list: []
-	};
-	async componentDidMount() {
-		// Fetch all items
-		let listResponse = api.readAll().then(items => {
-			if (items.message === 'unauthorized') {
-				if (isLocalHost()) {
-					alert(
-						'FaunaDB key is not unauthorized. Make sure you set it in terminal session where you ran npm start. Visit http://bit.ly/set-fauna-key for more info'
-					);
-				} else {
-					alert(
-						'FaunaDB key is not unauthorized. Verify the key FAUNADB_SECRET set in Netlify enviroment variables is correct'
-					);
-				}
-				return false;
-			}
+  state = {
+    list: []
+  };
+  async componentDidMount() {
+    // Fetch all items
+    let listResponse = api.readAll().then(items => {
+      if (items.message === "unauthorized") {
+        if (isLocalHost()) {
+          alert(
+            "FaunaDB key is not unauthorized. Make sure you set it in terminal session where you ran npm start. Visit http://bit.ly/set-fauna-key for more info"
+          );
+        } else {
+          alert(
+            "FaunaDB key is not unauthorized. Verify the key FAUNADB_SECRET set in Netlify enviroment variables is correct"
+          );
+        }
+        return false;
+      }
 
-			console.log('all items', items);
-			this.setState({
-				list: items
-			});
-		});
-	}
-	render() {
-		let myList;
-		if (this.state.list) {
-			console.log('hi', this.state.list);
-			myList = this.state.list.map(item => {
-				return item.data.title + item.data.date;
-			});
-		}
+      console.log("all items", items);
+      this.setState({
+        list: items
+      });
+    });
+  }
+  render() {
+    const { classes } = this.props;
 
-		return <div>{myList}</div>;
-	}
+    let myList;
+    if (this.state.list) {
+      console.log("hi", this.state.list);
+      myList = this.state.list.map(item => {
+        return item.data.title + item.data.date;
+      });
+
+      return (
+        <div className={classes.root}>
+          <GridList cellHeight={180} className={classes.gridList}>
+            <GridListTile key="Subheader" cols={2} style={{ height: "auto" }}>
+              <ListSubheader component="div">Food Diary</ListSubheader>
+            </GridListTile>
+            {this.state.list.map(item => (
+              <GridListTile key={item.ref["@ref"].id}>
+                <img src={item.data.fileBase} alt={item.data.title} />
+                <GridListTileBar
+                  title={item.data.title}
+                  subtitle={<span>date: {item.data.date}</span>}
+                />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      );
+
+      //   return <div>List is empty</div>;
+    } else {
+      return <div>List is empty</div>;
+    }
+  }
 }
 
-export default ListDiary;
+export default withStyles(styles)(ListDiary);
