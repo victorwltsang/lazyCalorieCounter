cb101968e13b92b2aa23bfd901184cb2994427bc
Victor Tsang <vtsang@netbase.com>
2018-11-03 17:03:35 -0700

    base 64

--- src/components/AddEntry/AddEntry.jsx
+++ src/components/AddEntry/AddEntry.jsx
@@ -3,12 +3,12 @@ import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { FilePond } from 'react-filepond';
+import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
-import { Typography, Button } from '@material-ui/core';
-import MenuItem from '@material-ui/core/MenuItem';
+import { Typography, Button, Divider } from '@material-ui/core';
+import FileBase64 from 'react-file-base64';
 import TextField from '@material-ui/core/TextField';
-import SaveIcon from '@material-ui/icons/Save';
+
 import moment from 'moment';
 
 const styles = theme => ({
@@ -35,15 +35,18 @@ const styles = theme => ({
 	ingredientsWrapper: {
 		marginTop: '8px',
 		padding: 20,
-		backgroundColor: 'transparent'
-		// border: '1px solid red'
+		backgroundColor: 'transparent',
+		border: '1px solid #b7b7b7'
 	}
 });
+
+const ingredients = [''];
 
 class AddEntry extends Component {
 	state = {
 		title: '',
-		date: ''
+		date: '',
+		files: []
 	};
 
 	componentDidMount() {
@@ -59,6 +62,22 @@ class AddEntry extends Component {
 			[name]: event.target.value
 		});
 	};
+
+	saveEntry = () => {
+		let entry = {
+			title: this.state.title,
+			date: this.state.date,
+			fileName: this.state.files.name,
+			fileBase: this.state.files.base64
+		};
+
+		console.log(entry);
+	};
+
+	getFiles(files) {
+		this.setState({ files: files });
+	}
+
 	render() {
 		const { classes } = this.props;
 		console.log(this.state);
@@ -99,22 +118,33 @@ class AddEntry extends Component {
 						</form>
 					</Grid>
 					<Grid item xs={12}>
-						<FilePond />
+						{/* <FilePond allowFileEncod="true" /> */}
+						<FileBase64 onDone={this.getFiles.bind(this)} />
 					</Grid>
 					<Grid item xs={12}>
 						<Typography variant="caption">Ingredients</Typography>
 						<Paper className={classes.ingredientsWrapper} />
 					</Grid>
 					<Grid item xs={12}>
+						<Divider />
+					</Grid>
+
+					<Grid item xs={12}>
 						<Grid container spacing={24}>
-							<Grid item xs={12} sm={6}>
-								<Button variant="contained" size="large" color="primary" className={classes.button}>
-									Save
-								</Button>
-							</Grid>
 							<Grid item xs={12} sm={6}>
 								<Button variant="contained" size="large" color="secondary" className={classes.button}>
 									Cancel
+								</Button>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<Button
+									variant="contained"
+									size="large"
+									color="primary"
+									className={classes.button}
+									onClick={this.saveEntry}
+								>
+									Save
 								</Button>
 							</Grid>
 						</Grid>
